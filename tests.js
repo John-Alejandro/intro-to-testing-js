3d7a4f90ece38d245590e4e68b1354af94faa835
john alejandro <john.e.alejandro01@gmail.com>
2021-08-24 12:33:01 -0500

    add third question and attemt at solution to intro-to -testing

--- tests.js
+++ tests.js
@@ -22,6 +22,10 @@ describe("sayhello", function() {
     });
     it("should return the string \"Hello, Jane!\" when executed", function() {
         expect(sayHello()).toBe("Hello, Jane!");
-    })
+    });
+    it("should return the string \"Hello Alex!\" when executed", function () {
+        expect(sayHello()).toBe("Hello, Alex!");
+    });
+
 })
 
